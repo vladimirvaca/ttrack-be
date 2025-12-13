6cf2c310567c697cb6a7098f259b239e3bae8716
Vladimir Vaca <ramvlay@gmail.com>
2025-12-13 01:19:11 -0500

    Added more fields in OpenAPI definition

--- src/main/java/com/rvladimir/Application.java
+++ src/main/java/com/rvladimir/Application.java
@@ -2,12 +2,19 @@ package com.rvladimir;
 
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @OpenAPIDefinition(
     info = @Info(
         title = "ttrack-be",
-        version = "0.0"
+        version = "1.0",
+        description = "Time tracking API",
+        contact = @Contact(
+            name = "rvladimir",
+            email = "ramvlay@gmail.com",
+            url = "https://rvladimir.com"
+        )
     )
 )
 public final class Application {
