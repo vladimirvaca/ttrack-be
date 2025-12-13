84891943cdc606ff63d834796faad42684c3b332
Vladimir Vaca <ramvlay@gmail.com>
2025-12-13 10:13:00 -0500

    Some improvements in controller Removed comments in bradle

--- src/main/java/com/rvladimir/web/rest/AuthResource.java
+++ src/main/java/com/rvladimir/web/rest/AuthResource.java
@@ -2,14 +2,31 @@ package com.rvladimir.web.rest;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Auth")
 @Controller("/auth")
-public final class AuthResource {
+public class AuthResource {
 
     @Get(produces = "application/json")
     public String index()  {
         return "{\"message\": \"Hello World\"}";
+    }
+
+    @Operation(
+        summary = "User Login",
+        description = "Authenticates a user and returns a login response"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "Successful login"
+    )
+    @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid credentials")
+    @Post(uri = "/login", produces = "application/json")
+    public String login()  {
+        return "{\"message\": \"Login endpoint\"}";
     }
 }
