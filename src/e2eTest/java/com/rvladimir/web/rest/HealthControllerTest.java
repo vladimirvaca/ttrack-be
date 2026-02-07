c988b6d9fab4cfea9cd7f92c50bb34a341a82249
Vladimir Vaca <ramvlay@gmail.com>
2026-02-07 12:06:17 -0500

    Enhance HealthControllerTest with PostgreSQL container support and property configuration

--- src/e2eTest/java/com/rvladimir/web/rest/HealthControllerTest.java
+++ src/e2eTest/java/com/rvladimir/web/rest/HealthControllerTest.java
@@ -2,19 +2,45 @@ package com.rvladimir.web.rest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.rvladimir.test.PostgresTestContainer;
+
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.micronaut.test.support.TestPropertyProvider;
 
 import jakarta.inject.Inject;
 
+import java.util.Map;
+
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 
 @MicronautTest
-class HealthControllerTest {
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class HealthControllerTest implements TestPropertyProvider {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = PostgresTestContainer.getInstance();
+
+    @Override
+    public Map<String, String> getProperties() {
+        return Map.of(
+            "datasources.default.url", postgres.getJdbcUrl(),
+            "datasources.default.username", postgres.getUsername(),
+            "datasources.default.password", postgres.getPassword(),
+            "datasources.default.driverClassName", postgres.getDriverClassName()
+        );
+    }
 
     @Inject
     @Client("/")
