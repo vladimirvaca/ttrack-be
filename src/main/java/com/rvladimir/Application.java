d394b0474964455d3108b75288f3197474901fcb
Vladimir Vaca <ramvlay@gmail.com>
2026-02-08 21:18:40 -0500

    Bump version to 0.2.14

--- src/main/java/com/rvladimir/Application.java
+++ src/main/java/com/rvladimir/Application.java
@@ -12,7 +12,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 @OpenAPIDefinition(
     info = @Info(
         title = "ttrack-be",
-        version = "0.2.13",
+        version = "0.2.14",
         description = "Time tracking API",
         contact = @Contact(
             name = "vladimirvaca",
