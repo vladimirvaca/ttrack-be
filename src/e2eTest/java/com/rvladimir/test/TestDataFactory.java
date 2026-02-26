05e765ec24d25ca003efa04403b982847b20d1dd
Vladimir Vaca <ramvlay@gmail.com>
2026-02-26 00:20:08 -0500

    Update user creation logic to derive nickname from email

--- src/e2eTest/java/com/rvladimir/test/TestDataFactory.java
+++ src/e2eTest/java/com/rvladimir/test/TestDataFactory.java
@@ -14,14 +14,22 @@ public final class TestDataFactory {
 
     private static final String DEFAULT_USER_NAME = "John";
     private static final String DEFAULT_USER_LASTNAME = "Doe";
-    private static final String DEFAULT_USER_NICKNAME = "johnd";
     private static final String DEFAULT_USER_PASSWORD = "hashedPassword";
+    private static final String AT_SIGN = "@";
     private static final int DEFAULT_BIRTH_YEAR = 1990;
     private static final int DEFAULT_BIRTH_MONTH = 5;
     private static final int DEFAULT_BIRTH_DAY = 15;
 
     private TestDataFactory() {
         // Private constructor to prevent instantiation
+    }
+
+    private static String nicknameFromEmail(String email) {
+        int atIndex = email.indexOf(AT_SIGN);
+        if (atIndex >= 0) {
+            return email.substring(0, atIndex);
+        }
+        return email;
     }
 
     /**
@@ -42,11 +50,12 @@ public final class TestDataFactory {
      * @return a new User instance
      */
     public static User createUser(String email, User.Role role) {
+        String nickname = nicknameFromEmail(email);
         return new User(
             null,
             DEFAULT_USER_NAME,
             DEFAULT_USER_LASTNAME,
-            DEFAULT_USER_NICKNAME,
+            nickname,
             LocalDate.of(DEFAULT_BIRTH_YEAR, DEFAULT_BIRTH_MONTH, DEFAULT_BIRTH_DAY),
             email,
             DEFAULT_USER_PASSWORD,
@@ -63,11 +72,12 @@ public final class TestDataFactory {
      * @return a new User instance with a hashed password
      */
     public static User createUserWithPassword(String email, String plainPassword) {
+        String nickname = nicknameFromEmail(email);
         return new User(
             null,
             DEFAULT_USER_NAME,
             DEFAULT_USER_LASTNAME,
-            DEFAULT_USER_NICKNAME,
+            nickname,
             LocalDate.of(DEFAULT_BIRTH_YEAR, DEFAULT_BIRTH_MONTH, DEFAULT_BIRTH_DAY),
             email,
             BCrypt.hashpw(plainPassword, BCrypt.gensalt()),
